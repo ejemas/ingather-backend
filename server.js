8ce365cc7da2b80c9bb0b3b989198206458ca5fa
Ejemas <maroejemas@gmail.com>
2026-05-07 10:21:54 +0100

    Fixed errors on profile pic update

--- server.js
+++ server.js
@@ -20,8 +20,8 @@ const io = socketIo(server, {
 
 // Middleware
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ extended: true, limit: '5mb' }));
 
 // Make io accessible to routes
 app.set('io', io);
